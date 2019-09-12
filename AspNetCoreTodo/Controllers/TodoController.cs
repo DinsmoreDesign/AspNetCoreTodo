6905a5e594206f65474c5c935c87793e4013fb28
Derek Dinsmore <derek.dinsmore@switchthink.com>
2019-09-12 14:08:57 -0700

    Adding way to mark items as done. * Added a new controller, interface and service for handling marking a todo item as Done * Added some JS to handle the submit on click

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using AspNetCoreTodo.Models;
 using AspNetCoreTodo.Services;
@@ -44,6 +45,26 @@ namespace AspNetCoreTodo.Controllers
             if (!successful)
             {
                 return BadRequest("Could not add item.");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [Route("MarkDone")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> MarkDone(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var successful = await _todoItemService.MarkDoneAsync(id);
+
+            if (!successful)
+            {
+                return BadRequest("Could not mark item as done.");
             }
 
             return RedirectToAction("Index");
--- AspNetCoreTodo/wwwroot/js/site.js
+++ AspNetCoreTodo/wwwroot/js/site.js
@@ -1,4 +1,21 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(() => {
 
-// Write your JavaScript code.
+    $('.done-checkbox').on('click', (e) => {
+
+        markCompleted(e.target);
+
+    });
+
+});
+
+function markCompleted(checkbox) {
+
+    checkbox.disabled = true;
+
+    const row = checkbox.closest('tr');
+    $(row).addClass('done');
+
+    const form = checkbox.closest('form');
+    form.submit();
+
+};
